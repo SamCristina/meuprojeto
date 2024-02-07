f1ca83c72693991478ea7f24e08fb011ffbc055b
SamCristina <samaracristinasousaandrade@gmail.com>
2024-02-07 17:11:45 -0300

    deletei um usuario

--- src/main.ts
+++ src/main.ts
@@ -68,6 +68,21 @@ app.put('/atualizarUsuario/:id', async (req, res) => {
 
 })
 
+app.delete('/deletarUsuario/:id', async(req, res) =>{
+    const id = req.params.id
+
+    try {
+        await firestore.deleteDoc(firestore.doc(db,'usuarios', id))
+
+        res.send('usuario deletado com sucesso!')
+         } catch (e) {
+            console.log('Error ao deletar usuario: ' + e)
+            res.status(500).send('Error ao deletar usuario: ' + e )
+        
+    }
+})
+
+
 app.listen(3000, function () {
     console.log("servidor rodando em http://localhost:3000");
 });
